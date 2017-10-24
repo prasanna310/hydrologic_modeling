ee78de735a3b2810b59416881546de78773fa698
UbuntuVM <dahal.prasanna@gmail.com>
2017-10-24 13:15:18 -0600

    run modelwith app's hydroshare

--- tethysapp/hydrologic_modeling/HDS_settings.py
+++ tethysapp/hydrologic_modeling/HDS_settings.py
@@ -1,7 +1,13 @@
 # set user account information for the ciwater Django api server
 # here to be able to run the tests
-USER_NAME = 'pdahal' # 'prasanna' #'nazmus'
-PASSWORD = 'pDahal2016'  #'dahal#2016' #'nazmus2015'
 
-HS_USER_NAME = 'prasanna_310'
-HS_PASSWORD = 'Hydrology12!@'
+# USER_NAME = 'pdahal' # 'prasanna' #'nazmus'
+# PASSWORD = 'pDahal2016'  #'dahal#2016' #'nazmus2015'
+
+
+USER_NAME = 'prasanna' #'nazmus'
+PASSWORD = 'dahal#2016' #'nazmus2015'
+
+
+HS_USER_NAME = ''
+HS_PASSWORD = ''
